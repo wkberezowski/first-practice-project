dfa234ec76c656679d532d67a02a073c0e101162
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-04 16:24:21 +0200

    added card and button component and started working on user input state

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,12 @@
+import classes from './Card.module.css';
+
 const Card = (props) => {
-  return;
+  return (
+    <div className={`${classes.card} ${props.className}`}>
+      {' '}
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
